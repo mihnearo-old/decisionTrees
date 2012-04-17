16e07499743802ad5f79ba2c6657b4be4df11e9a
Mihnea Olteanu <mihneao@gmail.com>
2012-04-16 20:36:49 -0700

    add accuracy evaluator

--- mlApp/Program.cs
+++ mlApp/Program.cs
@@ -8,6 +8,7 @@
     using mlLib.Arff;
     using mlLib.Classifier;
     using mlLib.DecisionTree;
+    using mlLib.Evaluator;
     using mlLib.Learner;
 
     class Program
@@ -17,6 +18,7 @@
             DateTime start = DateTime.Now;
 
             Instances data = null;
+            Instances testData = null;
             
             // read in the data from file
             using (StreamReader stream = new StreamReader(args[0]))
@@ -24,21 +26,28 @@
                 data = ArffReader.Read(stream, "Class");   
             }
 
+            testData = data;
+            if (2 <= args.Length)
+            {
+                using (StreamReader stream = new StreamReader(args[1]))
+                {
+                    data = ArffReader.Read(stream, "Class");
+                }
+            }
             
             // learn the tree
-            ID3Learner learner = new ID3Learner(0.0, true);
+            ID3Learner learner = new ID3Learner(0.99, true);
             Node decisionTree = learner.Learn(data);
 
             // output the tree
             System.Console.Out.WriteLine(decisionTree);
 
-            // classify the items
+            // evaluate the classifier
             DTClassifier classifier = new DTClassifier(decisionTree);
-            foreach (Instance example in data.Examples)
-            {
-                string predictedClass = classifier.Classify(example);
-                System.Console.Out.WriteLine("Classifier predictedClass=[{0}] item=[{1}]", predictedClass, example);
-            }
+            AccuracyEvaluator evaluator = new AccuracyEvaluator(classifier);
+            
+            double accuracy = evaluator.Evaluate(testData);
+            System.Console.Out.WriteLine("Accuracy: {0}", accuracy);
 
             System.Console.Out.WriteLine("Runtime: {0}", (DateTime.Now - start).ToString("c"));
             System.Console.Out.WriteLine("Press any key to exit");
---
+++ mlLib/Evaluator/AccuracyEvaluator.cs
@@ -0,0 +1,61 @@
+﻿namespace mlLib.Evaluator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using mlLib.Arff;
+    using mlLib.Classifier;
+
+    /// <summary>
+    /// Class implements an evaluator that measures the accuracy of a classifier
+    /// </summary>
+    public class AccuracyEvaluator
+    {
+        #region Private Members
+
+        private DTClassifier classifier = null;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Initializes a new instance of the AccuracyEvaluator class.
+        /// </summary>
+        /// <param name="classifier">classifier to evaluate</param>
+        public AccuracyEvaluator(DTClassifier classifier)
+        {
+            this.classifier = classifier;
+        }
+
+        /// <summary>
+        /// Computes the accuracy of the classifier on the data set passed as a parameter.
+        /// </summary>
+        /// <param name="data">test data to evaluate classifier on</param>
+        /// <returns>number between 0.0 and 1.0 representing the accuracy</returns>
+        public double Evaluate(Instances data)
+        {
+            if (null == data
+                || null == data.ClassAttribute)
+            {
+                throw new ArgumentNullException("data");
+            }
+
+            int correct = 0;
+            foreach (Instance example in data.Examples)
+            {
+                string predictedClass = this.classifier.Classify(example);
+                string actualClass = example.Data[data.ClassAttribute.Name];
+                if (predictedClass == actualClass)
+                {
+                    correct++;
+                }
+            }
+
+            return (double)correct / (double)data.Examples.Length;
+        }
+
+        #endregion
+    }
+}
